216b17778434ea203a89b20fd331713871a735dc
Alan Christian <ht93038@gmail.com>
2018-12-23 17:31:06 +0800

    add Login

---
+++ Login/registration.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registration</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<?php
+	require('db.php');
+    // If form submitted, insert values into the database.
+    if (isset($_REQUEST['loginID'])){
+		$loginID = stripslashes($_REQUEST['loginID']); // removes backslashes
+		$loginID = mysqli_real_escape_string($con,$loginID); //escapes special characters in a string
+		$pwd = stripslashes($_REQUEST['pwd']);
+		$pwd = mysqli_real_escape_string($con,$pwd);
+        $query = "INSERT into `user` (loginID, pwd) VALUES ('$loginID', '$pwd')";
+        $result = mysqli_query($con,$query);
+        if($result){
+            echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
+        }
+    }else{
+?>
+<div class="form">
+<h1>Registration</h1>
+<form name="registration" action="" method="post">
+<input type="text" name="loginID" placeholder="LoginID" required />
+<input type="password" name="pwd" placeholder="Password" required />
+<input type="submit" name="submit" value="Register" />
+</form>
+<br /><br />
+</div>
+<?php } ?>
+</body>
+</html>
