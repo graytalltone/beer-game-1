c8767a5035c0e470ccae68376b248aa033fe5c70
immwing <44217618+immwing@users.noreply.github.com>
2018-12-27 13:02:51 +0800

    Add files via upload
    new team

--- Login/index.php
+++ Login/index.php
@@ -1,28 +1,79 @@
-<?php
-/*
-Author: Javed Ur Rehman
-Website: http://www.allphptricks.com/
-*/
-
-include("auth.php"); //include auth.php file on all secure pages ?>
+<?php 
+include("auth.php"); //include auth.php file on all secure pages 
+require("db.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
 <title>Welcome Home</title>
 <link rel="stylesheet" href="css/style.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 <body>
-<div class="form">
-<p>Welcome <?php echo $_SESSION['loginID']; ?>!</p>
-<p>This is secure area.</p>
-<p><a href="dashboard.php">Dashboard</a></p>
+<p>Welcome <?php echo $_SESSION['loginID']; ?></p>
+<form id="beerGame" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+<?php 
+    //點了new team後資料庫team會增加一筆
+    echo '<input type="submit" value="New Team" name="new" id="new"/>';
+    if (isset($_POST['new'])) {
+        $sql = "insert into team (accost, rank, cost) values (0, 0 ,0)";
+        $stmt = mysqli_prepare($con, $sql);
+        mysqli_stmt_execute($stmt);
+    }
+    //從資料庫顯示目前隊伍數量
+    $sql = "select * from team;";
+    $stmt = mysqli_prepare($con, $sql);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    while ( $rs = mysqli_fetch_assoc($result)){
+        echo "<br>Team".$rs['tid'].get_buttons();
+    }
+    function get_buttons(){
+        $str='';
+        $btns = array(
+                    1=>'Factory',
+                    2=>'Distributor',
+                    3=>'Wholesaler',
+                    4=>'Retailer',);
+        while(list($k,$v)=each($btns)){
+            $str.='&nbsp;<input type="submit" value="'.$v.'" name="btn_'.$k.'" id="btn_'.$k.'"/>';
+        }
+        echo "<br>";
+        return $str;
+    }
+    // 選擇角色然後更新資料庫user裡的rid, tid
+    if(isset($_POST['btn_1'])){
+        $sql = "update user set rid=1 where loginID=?";
+        $stmt = mysqli_prepare($con, $sql); //prepare sql statement
+        mysqli_stmt_bind_param($stmt, 's' ,$_SESSION['loginID']); //bind parameters with variables
+        mysqli_stmt_execute($stmt);
+        echo "<br><br>Player A choosed!";
+    }
+    if(isset($_POST['btn_2'])){
+        $sql = "update user set rid=2 where loginID=?";
+        $stmt = mysqli_prepare($con, $sql); //prepare sql statement
+        mysqli_stmt_bind_param($stmt, 's', $_SESSION['loginID']); //bind parameters with variables
+        mysqli_stmt_execute($stmt);
+        echo "<br><br>Player B choosed!";
+    }
+    if(isset($_POST['btn_3'])){
+        $sql = "update user set rid=3 where loginID=?";
+        $stmt = mysqli_prepare($con, $sql); //prepare sql statement
+        mysqli_stmt_bind_param($stmt, 's', $_SESSION['loginID']); //bind parameters with variables
+        mysqli_stmt_execute($stmt);
+        echo "<br><br>Player C choosed!";
+    }
+    if(isset($_POST['btn_4'])){
+        $sql = "update user set rid=4 where loginID=? ";
+        $stmt = mysqli_prepare($con, $sql); //prepare sql statement
+        mysqli_stmt_bind_param($stmt, 's', $_SESSION['loginID']); //bind parameters with variables
+        mysqli_stmt_execute($stmt);
+        echo "<br><br>Player D choosed!";
+    }
+?>
+<br><br>
+</form>
 <a href="logout.php">Logout</a>
-
-
-<br /><br /><br /><br />
-<a href="http://www.allphptricks.com/simple-user-registration-login-script-in-php-and-mysqli/">Tutorial Link</a> <br /><br />
-For More Web Development Tutorials Visit: <a href="http://www.allphptricks.com/">AllPHPTricks.com</a>
-</div>
 </body>
 </html>
