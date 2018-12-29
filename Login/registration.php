3b43d0e8528452e009dcebeb1ebaca93bae29aa8
SkyYu93 <34836511+SkyYu93@users.noreply.github.com>
2018-12-30 00:54:23 +0800

    Add files via upload
    update LoginID exist

--- Login/registration.php
+++ Login/registration.php
@@ -14,11 +14,19 @@
 		$loginID = mysqli_real_escape_string($con,$loginID); //escapes special characters in a string
 		$pwd = stripslashes($_REQUEST['pwd']);
 		$pwd = mysqli_real_escape_string($con,$pwd);
+		
+		$sql = "select `loginID` from `user` where loginID='".$loginID."'";
+		$result = $con -> query($sql);
+		if(mysqli_num_rows($result)>0)
+        {
+            echo "<div class='form'><h3>You LoginID was exist.</h3><br/>Click here to <a href='registration.php'>Registration</a></div>";
+        }else{
         $query = "INSERT into `user` (loginID, pwd) VALUES ('$loginID', '$pwd')";
         $result = mysqli_query($con,$query);
-        if($result){
-            echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
-        }
+			if($result){
+				echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
+			}
+		}
     }else{
 ?>
 <div class="form">
