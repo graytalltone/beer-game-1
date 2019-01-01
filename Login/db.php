b2d0b1b323c87670446810f1f16a328d2236a28b
ckalan86924 <45911533+ckalan86924@users.noreply.github.com>
2019-01-02 06:16:52 +0800

    DB WRONG

--- Login/db.php
+++ Login/db.php
@@ -1,9 +1,7 @@
 <?php
-
-$con = mysqli_connect("localhost","root","","beer game");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+	$con = mysqli_connect("localhost","root","","beer_game");
+	// Check connection
+	if (mysqli_connect_errno()) {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
 ?>
