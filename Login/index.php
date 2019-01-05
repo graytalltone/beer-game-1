1de5d35f63a4d146036f4141e3bd2fb3d2bf98ba
immwing <44217618+immwing@users.noreply.github.com>
2019-01-05 17:19:31 +0800

    new

--- Login/index.php
+++ Login/index.php
@@ -1,36 +1,3 @@
 <?php
-require("dbconfig.php");
-checkLogin();
-require("teamModel.php");
-$userId = $_SESSION['loginID'];
+include("teamView1.php");
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Welcome Home</title>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-<p>Welcome <?php echo $userId; ?></p>
-<table id="beerGame">
-<form id="beerGame" action="teamControl.php" method="post">
-    Create a team<br>Team name:
-    <input type='text' name='teamName' value=''>
-    <input type='hidden' name='userId' value="<?php echo $userId?>">
-    <input type='submit' name='act' value='createTeam'>
-</form>
-<form id="beerGame" action="teamControl.php" method="post">
-    <br><br>Join a Team<br>Team name:
-    <input type='text' name='teamName' value=''>
-    <input type='hidden' name='userId' value="<?php echo $userId?>">
-    <input type='submit' name='act' value='joinTeam'>
-</form>
-<br><br>
-<?php include("teamView.php"); ?>
-<br><br>
-</form>
-</table>
-<a href="logout.php">Logout</a>
-</body>
-</html>
